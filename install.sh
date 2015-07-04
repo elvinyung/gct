cf3674caf23614c7a3ef07184eaebc14ec8c85bd
Elvin Yung <elvinyung1@gmail.com>
2015-07-03 20:10:26 -0700

    Rename gct to gct.sh for code highlighting on github

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-program_name="gct"
+program_name="gct.sh"
 install_dir="/usr/bin"
-install_path="$install_dir/$program_name"
+install_path="$install_dir/${program_name&&.sh}"
 
 cp "$program_name" "$install_path"
 chmod aguo+x "$install_path"
